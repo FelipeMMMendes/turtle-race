8535129c8812f0d99e47992e656d9239ad34226c
Felipe Martins <felipe_mmmachado@hotmail.com>
2022-10-30 21:45:40 -0300

    Implementado sistema de detecção do vencedor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #importando as bibliotecas
-from turtle import Turtle, Screen, width
+from turtle import Turtle, Screen, color
 
 #instancia objetos das classes Screen e Turtle
 tela = Screen()
@@ -8,11 +8,11 @@ tela = Screen()
 tela.setup(width=500, height=400)
 
 #faz com que apareca um prompt para que o usuario entre com a tartaruga que ele vai apostar
-escolhaUsuario = tela.textinput("Escolha de tartaruga: ","Cor: ")
+userChoice = tela.textinput("Escolha de tartaruga: ","Cor: ")
 #armazena o valor na variavel escolhaUsuario
 
 #variavel flag para manter a corrida acontecendo
-corridaAtiva = True
+corridaAtiva = False
 
 #lista contendo todas as cores, para ser acessadas e usadas no 
 # momento de instanciação das tartarugas
@@ -31,6 +31,10 @@ y=-100
 #variavel flag para controlar a posicao da cor
 colorId = 0
 
+#variavel flag para guardar as informacoes da tartaruga vencedora
+turtleWinner = Turtle()
+turtleWinner.hideturtle()
+
 for name in turtleNames:
     name = Turtle(shape="turtle")
     name.color(colors[colorId])
@@ -46,10 +50,29 @@ for name in turtleNames:
 #importando a funcao para escolher numeros aleatorios
 from random import randrange
 
+#ativando a corrida
+corridaAtiva=True
+
 #laco que vai manter as tartarugas correndo
 while corridaAtiva:
     for turtle in turtles:
+
+        #condicao para checar se a tartaruga ja chegou ao destino ou nao
+        if turtle.xcor() > 125:
+            corridaAtiva = False
+            turtleWinner = turtle
+            print(f"The winner was {turtleWinner.color()[0]}")
+
+
+        #laco que fica percorrendo todas as tartarugas e faz todas andarem um pouco
         turtle.forward(randrange(0, 11))
 
+
+
+if corridaAtiva == False:
+    if turtleWinner.color()[0] == userChoice:
+        print("You win!")
+    else:
+        print("You lose!")    
 
 tela.exitonclick()
